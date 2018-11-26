dbc8330abd91d27c20e1b4e8d620b1ef3fe3a3a7
Rodrigo Brechard <rodrigobrechard@gmail.com>
2018-11-26 11:33:03 +0100

    backpropagation and 3 dimesion matrix

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,7 @@
 import gym
 import numpy as np
 import random
+import matplotlib.pyplot as plt
 
 gym.envs.register(
 
@@ -16,10 +17,10 @@ env = gym.make("MountainCarMyEasyVersion-v0")
 
 GAMMA = 0.9
 
-n_position_state = 10
+n_position_state = 50
 position_state_array = np.linspace(-1.2, 0.6, num=n_position_state)
 
-n_velocity_state = 10
+n_velocity_state = 50
 velocity_state_array = np.linspace(-0.07, 0.07, num=n_velocity_state)
 
 # Row = position, Column = velocity
@@ -29,112 +30,145 @@ velocity_state_array = np.linspace(-0.07, 0.07, num=n_velocity_state)
 possible_actions = [0, 1, 2]
 
 # Rows = state, Column = actions
-q_table = np.zeros((n_position_state * n_velocity_state, len(possible_actions)))
+q_table = np.zeros((n_position_state, n_velocity_state, len(possible_actions)))
 
-times_action_executed = np.zeros((n_position_state * n_velocity_state, len(possible_actions)))
+times_action_executed = np.zeros((n_position_state, n_velocity_state, len(possible_actions)))
+
+policy = np.zeros((n_position_state, n_velocity_state), dtype=int)
+v_table = np.zeros((n_position_state, n_velocity_state), dtype=int)
 
 
-def get_row_of_state(observation):
+def get_position_velocity_of_state(observation):
     """
-        Get the row that corresponds to a particular observation, considering that the q_table has been designed in the
-        way that every row is represents a state.
+        Get the position and velocity discretizated from the observation vector
         :param observation: two positions array, where position 0 has the position and position 1 the velocity
-        :return: state row in the q_table
+        :return: state in the q_table
         """
     # Discretize the continuous value of the observations, action and position
     # Position 0 = position, Position 1 = velocity
     obs = (np.digitize(observation[0], position_state_array) - 1,
            np.digitize(observation[1], velocity_state_array) - 1)
-
-    row = obs[0] * n_velocity_state + obs[1]
-    if row > n_velocity_state * n_position_state:
-        print("error")
-    return row
+    position = obs[0]
+    velocity = obs[1]
+    return position, velocity
 
 
-# observation, Row = velocity, Column = position
-# q_table = {}
-#
-# print("a")
-# actions = [0, 1, 2]
-#
-# observation = [0, 0]
-#
-#
-#
-#
-# # At the beggining we use the epsilon greedy strategy
+def update_policy_and_v_table(pos, vel):
+    policy[pos, vel] = np.argmax(q_table[pos, vel])
+    v_table[pos, vel] = max(q_table[pos, vel])
+
+
+def backpropagate(history):
+    """
+    Backpropagate the q_value through the states executed. The history array has in every position the history saved
+    in a tuple: [pos, vel, action, reward]
+    :return:
+    """
+    for pos, hist in reversed(list(enumerate(history))):
+        if pos == len(history) - 1:
+            next_state_pos = hist[0]
+            next_state_vel = hist[1]
+            continue
+        learning_rate = 1
+
+        current_pos = hist[0]
+        current_vel = hist[1]
+        action = hist[2]
+        reward = hist[3]
+        current_state_q_value = q_table[current_pos, current_vel, action]
+
+        new_q_value = current_state_q_value + learning_rate * \
+                      (reward + GAMMA * max(q_table[next_state_pos, next_state_vel]) - current_state_q_value)
+
+        q_table[current_pos, current_vel, action] = new_q_value
+
+        update_policy_and_v_table(current_pos, current_vel)
+
+        next_state_pos = current_pos
+        next_state_vel = current_vel
 
 
 def q_learning(times_repeat):
     # In the beginning, this rate must be at its highest value, because we don’t know anything about
     # the values in Q-table.
     # Therefore we set it to 1 so that it is only exploration and we choose a random state
-    epsilon = 1
-
-    current_state = get_row_of_state(env.reset())
-
-    # print(current_state)
-    epsilons = []
-    epsilons.append(epsilon)
-    done = False
-    timesteps = 0
 
     for i in range(times_repeat):
-        epsilon = calculate_until_finish(current_state, done, epsilon, False)
+        calculate_until_finish(False, i)
 
-    calculate_until_finish(current_state, done, epsilon, True)
+    epsilon = calculate_until_finish(True, times_repeat)
 
     return epsilon
 
 
-def calculate_until_finish(current_state, done, epsilon, draw):
+def calculate_until_finish(draw, n_times):
+    current_pos, current_vel = get_position_velocity_of_state(env.reset())
     timesteps = 0
     epsilon = 1
-    # print("AAAAAAAAAAAAAAAA")
     done = False
-    env.reset()
-    # while not done:
-    for i in range(1000):
+    history = []
+
+    while not done:
         timesteps += 1
         if draw:
+            # env.render(file_path='./mountain_car.gif', mode='gif')
             env.render()
-            action = np.argmax(q_table[current_state])
-
+            action = policy[current_pos, current_vel]
         else:
-
             if random.random() > epsilon:  # Exploitation, choose the best action
-                action = np.argmax(q_table[current_state])
+                action = policy[current_pos, current_vel]
             else:  # Exploration, we choose a random action
                 action = random.choice(possible_actions)
 
         # action = env.action_space.sample()
-
         observation, reward, done, info = env.step(action)
+
+        # if done:
+        #     reward = 5
+            # print("DONE:", reward)
+            # break
+        # else:
+            # print("NOT DONE:", reward)
         # print("Timestep", timesteps, "execute action", action, "epsilon", epsilon, "reward", reward)
 
-        new_state = get_row_of_state(observation)
+        new_pos, new_vel = get_position_velocity_of_state(observation)
 
-        current_q_value = q_table[current_state][action]
+        current_q_value = q_table[current_pos, current_vel][action]
 
         # Update the counters table
-        times_action_executed[current_state][action] += 1
+        times_action_executed[current_pos, current_vel][action] += 1
 
         # λ^n = n^−α
-        learning_rate = times_action_executed[current_state][action] ** -0.9
+        learning_rate = times_action_executed[current_pos, current_vel][action] ** -0.9
 
         # Update the q values table
-        q_table[current_state][action] = current_q_value + learning_rate * \
-                                         (reward + GAMMA * max(q_table[new_state]) - current_q_value)
+        new_q_value = current_q_value + learning_rate * \
+                      (reward + GAMMA * max(q_table[new_pos, new_vel]) - current_q_value)
 
+        q_table[current_pos, current_vel, action] = new_q_value
+
+        history.append([current_pos, current_vel, action, reward])
+
+        update_policy_and_v_table(current_pos, current_vel)
         # Add the value to the table for the convergence plot
-        # q_table_all_values[current_state][action].append(value)
+        # q_table_all_values[current_pos, current_vel][action].append(value)
 
-        current_state = new_state
+        current_pos, current_vel = new_pos, new_vel
 
-        epsilon -= 1 / 100
+        epsilon = 1 / timesteps ** 0.01
     print("Done in", timesteps, "timesteps")
+
+    # if draw:
+    # else:
+    fig, ax = plt.subplots()
+    im = ax.pcolormesh(v_table)
+    fig.colorbar(im)
+    # plt.show()
+    # else:
+    plt.savefig("img/" + str(n_times) + ".png")
+    backpropagate(history)
+
     return epsilon
 
 
-q_learning(10000)
+q_learning(1000)
