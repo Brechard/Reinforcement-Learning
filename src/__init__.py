learning rate fixed in backpropagation

src/__init__.py
@@ -30,7 +30,7 @@ velocity_state_array = np.linspace(-0.07, 0.07, num=n_velocity_state)
 possible_actions = [0, 1, 2]
 
 # Rows = state, Column = actions
-q_table = np.zeros((n_position_state, n_velocity_state, len(possible_actions)))
+q_table = np.random.rand(n_position_state, n_velocity_state, len(possible_actions))
 
 times_action_executed = np.zeros((n_position_state, n_velocity_state, len(possible_actions)))
 
@@ -69,13 +69,14 @@ def backpropagate(history):
             next_state_pos = hist[0]
             next_state_vel = hist[1]
             continue
-        learning_rate = 1
 
         current_pos = hist[0]
         current_vel = hist[1]
         action = hist[2]
         reward = hist[3]
         current_state_q_value = q_table[current_pos, current_vel, action]
+
+        learning_rate = times_action_executed[current_pos, current_vel][action] ** -0.9
 
         new_q_value = current_state_q_value + learning_rate * \
                       (reward + GAMMA * max(q_table[next_state_pos, next_state_vel]) - current_state_q_value)
@@ -102,13 +103,12 @@ def q_learning(times_repeat):
 
 
 def calculate_until_finish(draw, n_times):
-    current_pos, current_vel = get_position_velocity_of_state(env.reset())
-    timesteps = 0
-    epsilon = 1
-    done = False
-    history = []
+    current_pos, current_vel, done, epsilon, history, times_action_executed, timesteps = restart_environment()
 
-    while not done:
+    n_max_timesteps = 1000
+
+    while not done and timesteps < n_max_timesteps:
+
         timesteps += 1
         if draw:
             # env.render(file_path='./mountain_car.gif', mode='gif')
@@ -123,27 +123,38 @@ def calculate_until_finish(draw, n_times):
         # action = env.action_space.sample()
         observation, reward, done, info = env.step(action)
 
-        # if done:
-        #     reward = 5
-            # print("DONE:", reward)
-            # break
+        # if reward != -1:
+        #     print("------------------------------------")
+        # print("DONE:", reward)
+        # break
+
         # else:
-            # print("NOT DONE:", reward)
+        # print("NOT DONE:", reward)
         # print("Timestep", timesteps, "execute action", action, "epsilon", epsilon, "reward", reward)
 
         new_pos, new_vel = get_position_velocity_of_state(observation)
 
-        current_q_value = q_table[current_pos, current_vel][action]
+        # if done:
+        #     reward = 100
+
+        current_q_value = q_table[current_pos, current_vel, action]
 
         # Update the counters table
         times_action_executed[current_pos, current_vel][action] += 1
 
-        # λ^n = n^−α
+        # λ = n^−α
         learning_rate = times_action_executed[current_pos, current_vel][action] ** -0.9
 
         # Update the q values table
+        v = current_q_value + learning_rate * (reward + GAMMA * max(q_table[new_pos, new_vel]) - current_q_value)
+
+        new_state_q_value = max(q_table[new_pos, new_vel])
+
+        if done:
+            new_state_q_value = 0
+
         new_q_value = current_q_value + learning_rate * \
-                      (reward + GAMMA * max(q_table[new_pos, new_vel]) - current_q_value)
+                      (reward + GAMMA * new_state_q_value - current_q_value)
 
         q_table[current_pos, current_vel, action] = new_q_value
 
@@ -155,20 +166,41 @@ def calculate_until_finish(draw, n_times):
 
         current_pos, current_vel = new_pos, new_vel
 
-        epsilon = 1 / timesteps ** 0.01
-    print("Done in", timesteps, "timesteps")
+        # epsilon = 1 / timesteps ** 0.01
+        epsilon -= 1 / n_max_timesteps
+
+        # print("epsilon", epsilon)
 
     # if draw:
     # else:
-    fig, ax = plt.subplots()
-    im = ax.pcolormesh(v_table)
-    fig.colorbar(im)
+
+    if n_times % 10 == 0:
+        print("Iteration", n_times, "done in", timesteps, "timesteps")
+        fig, ax = plt.subplots()
+        im = ax.pcolormesh(velocity_state_array, position_state_array, v_table[:, :], vmin=0, vmax=-8)
+        ax.set_xlabel("Velocity")
+        ax.set_ylabel("Position")
+        ax.set_title("Iteration " + str(n_times))
+        fig.colorbar(im)
+        plt.savefig("img/" + str(n_times) + ".png")
+
     # plt.show()
     # else:
-    plt.savefig("img/" + str(n_times) + ".png")
+
     backpropagate(history)
 
     return epsilon
 
 
+def restart_environment():
+    current_pos, current_vel = get_position_velocity_of_state(env.reset())
+    timesteps = 0
+    epsilon = 1
+    done = False
+    history = []
+    # times_action_executed = np.zeros((n_position_state, n_velocity_state, len(possible_actions)))
+    return current_pos, current_vel, done, epsilon, history, times_action_executed, timesteps
+
+
 q_learning(1000)
+# plt.show()